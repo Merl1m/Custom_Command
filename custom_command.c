7d32e33732cbe3c2ba6cbdad64e51653fa50b8ec
Merlin the Mage <110106556+Merl1m@users.noreply.github.com>
2023-02-08 11:01:23 +0530

    Add files via upload

---
+++ custom_command.c
@@ -0,0 +1,11 @@
+// Create permenant alias for commands on linux command line
+// Created by Muhammed Farhan NP
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    system("ls -la"); //Customised command
+    return 0;
+}
